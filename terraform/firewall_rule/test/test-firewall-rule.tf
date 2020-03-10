10f20c3872c8403b2e278bf6369497a538179c57
Henry Ferrara <ferrara@broadinstitute.org>
2020-03-10 17:46:34 -0400

    fix formatting

--- terraform/firewall_rule/test/test-firewall-rule.tf
+++ terraform/firewall_rule/test/test-firewall-rule.tf
@@ -6,7 +6,7 @@
 module "test-firewall-rule" {
   source = "../"
 
-  firewall_rule_name = "test-rule"
+  firewall_rule_name          = "test-rule"
   firewall_rule_source_ranges = ["192.168.1.1/32"]
   firewall_rule_target_tags   = ["test-target-firewall-rule"]
   firewall_rule_ports         = ["443","22"]
