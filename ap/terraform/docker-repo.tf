8cd3e235ab2363016eaefc1b12c0711a7c6a7210
Henry Ferrara <ferrara@broadinstitute.org>
2021-04-29 12:12:49 -0400

    fix fmt

--- ap/terraform/docker-repo.tf
+++ ap/terraform/docker-repo.tf
@@ -2,30 +2,30 @@
 resource "google_artifact_registry_repository" "hjf-docker-public" {
   provider = google-beta.automatic-potato-hjf
 
-#  location = "us-central1"
-  location = "US"
+  #  location = "us-central1"
+  location      = "US"
   repository_id = "hjf-docker-public"
-  description = "HJF Docker public repo"
-  format = "DOCKER"
+  description   = "HJF Docker public repo"
+  format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository_iam_binding" "hjf-docker-public-reader" {
-  provider = google-beta.automatic-potato-hjf
-  project = google_artifact_registry_repository.hjf-docker-public.project
-  location = google_artifact_registry_repository.hjf-docker-public.location
+  provider   = google-beta.automatic-potato-hjf
+  project    = google_artifact_registry_repository.hjf-docker-public.project
+  location   = google_artifact_registry_repository.hjf-docker-public.location
   repository = google_artifact_registry_repository.hjf-docker-public.name
-  role = "roles/artifactregistry.reader"
+  role       = "roles/artifactregistry.reader"
   members = [
     "allUsers",
   ]
 }
 
 resource "google_artifact_registry_repository_iam_binding" "hjf-docker-public-repo-admin" {
-  provider = google-beta.automatic-potato-hjf
-  project = google_artifact_registry_repository.hjf-docker-public.project
-  location = google_artifact_registry_repository.hjf-docker-public.location
+  provider   = google-beta.automatic-potato-hjf
+  project    = google_artifact_registry_repository.hjf-docker-public.project
+  location   = google_artifact_registry_repository.hjf-docker-public.location
   repository = google_artifact_registry_repository.hjf-docker-public.name
-  role = "roles/artifactregistry.repoAdmin"
+  role       = "roles/artifactregistry.repoAdmin"
   members = [
     "group:devunll@broadinstitute.org",
     "serviceAccount:${data.google_project.automatic-potato-hjf.number}@cloudbuild.gserviceaccount.com",
@@ -35,30 +35,30 @@ resource "google_artifact_registry_repository_iam_binding" "hjf-docker-public-re
 resource "google_artifact_registry_repository" "hjf-docker-private" {
   provider = google-beta.automatic-potato-hjf
 
-#  location = "us-central1"
-  location = "US"
+  #  location = "us-central1"
+  location      = "US"
   repository_id = "hjf-docker-private"
-  description = "HJF Docker public repo"
-  format = "DOCKER"
+  description   = "HJF Docker public repo"
+  format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository_iam_binding" "hjf-docker-private-reader" {
-  provider = google-beta.automatic-potato-hjf
-  project = google_artifact_registry_repository.hjf-docker-private.project
-  location = google_artifact_registry_repository.hjf-docker-private.location
+  provider   = google-beta.automatic-potato-hjf
+  project    = google_artifact_registry_repository.hjf-docker-private.project
+  location   = google_artifact_registry_repository.hjf-docker-private.location
   repository = google_artifact_registry_repository.hjf-docker-private.name
-  role = "roles/artifactregistry.reader"
+  role       = "roles/artifactregistry.reader"
   members = [
     "domain:broadinstitute.org",
   ]
 }
 
 resource "google_artifact_registry_repository_iam_binding" "hjf-docker-private-repo-admin" {
-  provider = google-beta.automatic-potato-hjf
-  project = google_artifact_registry_repository.hjf-docker-private.project
-  location = google_artifact_registry_repository.hjf-docker-private.location
+  provider   = google-beta.automatic-potato-hjf
+  project    = google_artifact_registry_repository.hjf-docker-private.project
+  location   = google_artifact_registry_repository.hjf-docker-private.location
   repository = google_artifact_registry_repository.hjf-docker-private.name
-  role = "roles/artifactregistry.repoAdmin"
+  role       = "roles/artifactregistry.repoAdmin"
   members = [
     "group:devunll@broadinstitute.org",
     "serviceAccount:${data.google_project.automatic-potato-hjf.number}@cloudbuild.gserviceaccount.com",
