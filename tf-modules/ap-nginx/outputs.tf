881b71470ce672ac0a82c9cd6d9d8ea9624e6e03
Henry Ferrara <ferrara@broadinstitute.org>
2021-05-20 12:19:34 -0400

    fix module ref

--- tf-modules/ap-nginx/outputs.tf
+++ tf-modules/ap-nginx/outputs.tf
@@ -1,9 +1,9 @@
 output "helm-values" {
   value = templatefile("${path.root}/helm-values.tpl",
     {
-      "cert-name"  = module.static_ip.cert.name
-      "ip-name"    = module.static_ip.static_ip.name
-      "ip-address" = module.static_ip.static_ip.address
+      "cert-name"  = module.static-ip.cert.name
+      "ip-name"    = module.static-ip.static_ip.name
+      "ip-address" = module.static-ip.static_ip.address
     }
   )
 }
