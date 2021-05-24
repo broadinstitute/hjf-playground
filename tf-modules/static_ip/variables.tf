923f02ea2622f6c8f93c92ba138daa5faaf6eec9
Henry Ferrara <ferrara@broadinstitute.org>
2021-05-24 08:56:39 -0400

    update module

--- tf-modules/static_ip/variables.tf
+++ tf-modules/static_ip/variables.tf
@@ -2,6 +2,7 @@ variable "dns_zone" {
   description = "resource google_dns_managed_zone.zone"
   type        = string
 }
+
 variable "site_name" {
   description = "name of the site to be added"
   type        = string
