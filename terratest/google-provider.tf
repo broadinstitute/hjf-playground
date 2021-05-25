4358b0b5986eb51362dc2e358a179126e1fcbe3d
Henry Ferrara <ferrara@broadinstitute.org>
2021-05-25 08:30:14 -0400

    fix fmt

--- terratest/google-provider.tf
+++ terratest/google-provider.tf
@@ -1,15 +1,15 @@
 
 # Configure the Google Cloud provider
 provider "google" {
-    alias       = "eddy"
-    project     = var.google_project
-    region      = var.region
-    version     = "~> 3.2"
+  alias   = "eddy"
+  project = var.google_project
+  region  = var.region
+  version = "~> 3.2"
 }
 
 provider "google-beta" {
-    alias       = "eddy"
-    project     = var.google_project
-    region      = var.region
-    version     = "~> 3.2"
+  alias   = "eddy"
+  project = var.google_project
+  region  = var.region
+  version = "~> 3.2"
 }
